24213a4095a58334ab93179facc4a169cbc97101
Sunyoung <78402560+sunnyoi-54@users.noreply.github.com>
2024-06-02 03:22:51 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.09 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,25 +3,21 @@ import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         StringBuilder answer = new StringBuilder();
+        
         int numLen = number.length();
-        int ansLen = numLen - k;
-        int scope = numLen - (ansLen - 1);
+        int scope = k + 1;
         int first = 0;
-        
-        int[] digits = new int[numLen];
-        for(int i=0; i<numLen; i++) 
-                digits[i] = number.charAt(i) - '0';
-        
         int index = 0;
-        while(answer.length() != ansLen){
-            int max = 0;
-            for(int i = first; i<scope; i++){
-                if (digits[i] > max) {
-                    max = digits[i];
+        
+        while(answer.length() != numLen - k){
+            char max = 0;
+            for(int i = first; i < scope; i++){
+                if (number.charAt(i) > max) {
+                    max = number.charAt(i);
                     index = i;
                 }
             }
-            answer.append(Integer.toString(max));
+            answer.append(max);
             first = index + 1;
             if(index == scope){
                 answer.append(answer.toString().substring(first, numLen));
@@ -30,6 +26,4 @@ class Solution {
         }
         return answer.toString();
     }
-    
-     
 }
